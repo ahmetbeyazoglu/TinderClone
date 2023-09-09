32a685035dde51d63c2eaa7acfed0f05f85efb2a
herpestes <ahmetbeyazoglu@gmail.com>
2023-09-09 13:25:54 +0300

    PlaceHolders And NavHost Setup

--- app/src/main/java/com/herpestes/tinderclone/MainActivity.kt
+++ app/src/main/java/com/herpestes/tinderclone/MainActivity.kt
@@ -9,9 +9,26 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.herpestes.tinderclone.ui.SwipeCards
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.herpestes.tinderclone.ui.*
 import com.herpestes.tinderclone.ui.theme.TinderCloneTheme
 
+
+sealed class DestinationScreen(val route: String){
+    object Signup : DestinationScreen("signup")
+    object Login : DestinationScreen("login")
+    object Profile : DestinationScreen("profile")
+    object Swipe : DestinationScreen("swipe")
+    object ChatList : DestinationScreen("chatlist")
+    object SingleChat: DestinationScreen("singleChat/{chatId}") {
+        fun createRoute(id: String) = "SingleChat/$id"
+    }
+
+
+
+}
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +39,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SwipeCards()
-
+                    SwipeAppNavigation()
                 }
             }
         }
     }
 }
+
+@Composable
+fun SwipeAppNavigation(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = DestinationScreen.Swipe.route){
+        composable(DestinationScreen.Signup.route){
+            SignupScreen()
+        }
+        composable(DestinationScreen.Login.route){
+            LoginScreen()
+        }
+        composable(DestinationScreen.Profile.route){
+            ProfileScreen()
+        }
+        composable(DestinationScreen.Swipe.route){
+            SwipeCards()
+        }
+        composable(DestinationScreen.ChatList.route){
+            ChatListScreen()
+        }
+        composable(DestinationScreen.SingleChat.route){
+            SingleChatScreen(chatId = "123")
+        }
+    }
+}
+
 
